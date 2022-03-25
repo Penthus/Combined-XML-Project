4c42bc145ea9922c637c3b9293855a49b9f24018
Penthus <eblisbm@hotmail.com>
2022-03-25 20:43:37 +0800

    Initial version of a more robust xml parser. -It will read the xml file and determine the Root note. -It will then run through a switch statement to see method to use
    -Still need to add extra checking so it does items, ships etc

--- XML_Serializer.cs
+++ XML_Serializer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml;
 using System.Xml.Linq;
 using XML_Serializer.XML;
 
@@ -13,16 +14,55 @@ namespace Combined_XML_Program
         {
             var basePath = AppDomain.CurrentDomain.BaseDirectory;
 
-            // InventoryXML(basePath, "Inventory"); //Broken Somehow?
+            string[] fileEntries = Directory.GetFiles(basePath + "\\XML\\", "*.xml");
+            List<XmlTypes> xmlTypes = GetXmlTypes(fileEntries);
+
+            foreach (var xmlType in xmlTypes)
+            {
+                switch (xmlType.Root)
+                {
+                    case "GALAXY":
+                        SpawnListXML(basePath, "Spawn", xmlType.Path);
+                        break;
+                    default:
+                        Console.WriteLine($"{xmlType} is not recognised");
+                        break;
+                }
+            }
+            //InventoryXML(basePath, "Inventory"); //Broken Somehow?
 
             //RepairKitsXML(basePath, "Repair Kits");
 
-            // StationKitsXML(basePath, "Station Kits");
+            //StationKitsXML(basePath, "Station Kits");
 
-            SpawnListXML(basePath, "Spawn");
+            //SpawnListXML(basePath, "Spawn", xmlTypes);
 
         }
 
+        private static List<XmlTypes> GetXmlTypes(string[] fileEntries)
+        {
+            List<XmlTypes> valueCollection = new List<XmlTypes>();
+            foreach (var fileEntry in fileEntries)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(fileEntry);
+                string Xml = doc.InnerXml;
+                using (var reader = XmlReader.Create(new StringReader(Xml)))
+                {
+                    reader.MoveToContent();
+                    if (reader.Name == "GALAXY")
+                    {
+                        Console.WriteLine(reader);
+                        valueCollection.Add(new XmlTypes(fileEntry, reader.Name, Xml));
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{reader.Name}: is not a recognised Root Node");
+                    }
+                }
+            }
+            return valueCollection;
+        }
         private static void InventoryXML(string? basePath, string? typePath)
         {
             string[] fileEntries = Directory.GetFiles(basePath + typePath + @"/", "*.xml");
@@ -138,78 +178,61 @@ namespace Combined_XML_Program
             }
         }
 
-        private static void SpawnListXML(string? basePath, string? typePath)
+        private static void SpawnListXML(string? basePath, string? typePath, string? path)
         {
-            string[] fileEntries = Directory.GetFiles(basePath + typePath + @"\", "*.xml");
-            foreach (var entry in fileEntries)
+
+            var Xml = SS_Serializer_SpawnList.GALAXY.LoadFromFile(path);
+            var s1 = path.Replace(basePath, "");
+            var s2 = s1.Replace(typePath, "");
+            var s3 = s2.Replace('\\', ' ');
+            s3 = s3.Trim();
+            var filename = s3.Remove(s3.IndexOf('.'));
+            var Combined = basePath + typePath + $@"\Combined.txt";
+            var Path = basePath + typePath + $@"\{filename}-text.txt";
+            var Names = basePath + typePath + $@"\{filename}-names.txt";
+
+            if (File.Exists(Path))
             {
-                var Xml = SS_Serializer_SpawnList.GALAXY.LoadFromFile(entry);
-                var s1 = entry.Replace(basePath, "");
-                var s2 = s1.Replace(typePath, "");
-                var s3 = s2.Replace('\\', ' ');
-                s3 = s3.Trim();
-                var filename = s3.Remove(s3.IndexOf('.'));
-                var Combined = basePath + typePath + $@"\Combined.txt";
-                var Path = basePath + typePath + $@"\{filename}-text.txt";
-                var Names = basePath + typePath + $@"\{filename}-names.txt";
+                File.Delete(Path);
+            }
 
-                if (File.Exists(Path))
+            if (File.Exists(Names))
+            {
+                File.Delete(Names);
+            }
+
+
+            //this needs to be made to work for various schema
+            foreach (var g in Xml.SPAWNLIST)
+            {
+                foreach (var s in g.SPAWNOBJ)
                 {
-                    File.Delete(Path);
-                }
-
-                if (File.Exists(Names))
-                {
-                    File.Delete(Names);
-                }
-
-
-                //this needs to be made to work for various schema
-                foreach (var g in Xml.SPAWNLIST)
-                {
-                    foreach (var s in g.SPAWNOBJ)
+                    foreach (var i in s.SPAWNAMBUSH)
                     {
-                        foreach (var i in s.SPAWNAMBUSH)
+                        foreach (var j in i.SPAWNER)
                         {
-                            foreach (var j in i.SPAWNER)
+                            if (j.LOCKOUTNAME?.Length > 0)
                             {
-                                if (j.LOCKOUTNAME?.Length > 0)
+                                Console.WriteLine($"Name: {j.NAMEDMOB}, Lockout Name: {j.LOCKOUTNAME}");
+                                File.AppendAllText(Path,
+                                    $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME} {Environment.NewLine}");
+                                File.AppendAllText(Names, $"{j.NAMEDMOB} {Environment.NewLine}");
+                                foreach (var ambushers in i.AMBUSHERS_CONFIGURATION)
                                 {
-                                    Console.WriteLine($"Name: {j.NAMEDMOB}, Lockout Name: {j.LOCKOUTNAME}");
-                                    File.AppendAllText(Path,
-                                        $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME} {Environment.NewLine}");
-                                    File.AppendAllText(Names, $"{j.NAMEDMOB} {Environment.NewLine}");
-                                    foreach (var ambushers in i.AMBUSHERS_CONFIGURATION)
+                                    foreach (var ambusher in ambushers.AMBUSHER)
                                     {
-                                        foreach (var ambusher in ambushers.AMBUSHER)
-                                        {
-                                            Console.WriteLine(ambusher.HULL_CONFIGURATION);
-                                            File.AppendAllText(Names,
-                                                $"{ambusher.HULL_CONFIGURATION} {Environment.NewLine}");
-                                        }
+                                        Console.WriteLine(ambusher.HULL_CONFIGURATION);
+                                        File.AppendAllText(Names,
+                                            $"{ambusher.HULL_CONFIGURATION} {Environment.NewLine}");
                                     }
                                 }
                             }
                         }
+                    }
 
-                        foreach (var i in s.SPAWNBOSS)
-                        {
-                            foreach (var j in i.SPAWNER)
-                            {
-                                if (j.LOCKOUTNAME?.Length > 0)
-                                {
-                                    Console.WriteLine(
-                                        $"Name: {j.NAMEDMOB}, Lockout Name: {j.LOCKOUTNAME}, Lockout Time: {j.period}|{j.LOCKOUTFIXED}, Ship Config: {j.SHIPCONFIG}");
-                                    File.AppendAllText(Path,
-                                        $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME}, Lockout Time, {j.period}, Ship Config, {j.SHIPCONFIG} {Environment.NewLine}");
-                                    File.AppendAllText(Names, $"{j.NAMEDMOB} {Environment.NewLine}");
-                                    File.AppendAllText(Combined,
-                                        $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME}, Lockout Time, {j.period}, Ship Config, {j.SHIPCONFIG} {Environment.NewLine}");
-                                }
-                            }
-                        }
-
-                        foreach (var j in s.SPAWNER)
+                    foreach (var i in s.SPAWNBOSS)
+                    {
+                        foreach (var j in i.SPAWNER)
                         {
                             if (j.LOCKOUTNAME?.Length > 0)
                             {
@@ -221,8 +244,22 @@ namespace Combined_XML_Program
                                 File.AppendAllText(Combined,
                                     $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME}, Lockout Time, {j.period}, Ship Config, {j.SHIPCONFIG} {Environment.NewLine}");
                             }
-
                         }
+                    }
+
+                    foreach (var j in s.SPAWNER)
+                    {
+                        if (j.LOCKOUTNAME?.Length > 0)
+                        {
+                            Console.WriteLine(
+                                $"Name: {j.NAMEDMOB}, Lockout Name: {j.LOCKOUTNAME}, Lockout Time: {j.period}|{j.LOCKOUTFIXED}, Ship Config: {j.SHIPCONFIG}");
+                            File.AppendAllText(Path,
+                                $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME}, Lockout Time, {j.period}, Ship Config, {j.SHIPCONFIG} {Environment.NewLine}");
+                            File.AppendAllText(Names, $"{j.NAMEDMOB} {Environment.NewLine}");
+                            File.AppendAllText(Combined,
+                                $"Name, {j.NAMEDMOB}, Lockout Name, {j.LOCKOUTNAME}, Lockout Time, {j.period}, Ship Config, {j.SHIPCONFIG} {Environment.NewLine}");
+                        }
+
                     }
                 }
             }
---
+++ XmlTypes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combined_XML_Program
+{
+    class XmlTypes
+    {
+        public string Path { get; set; }
+        public string Root { get; set; }
+        public string Xml { get; set; }
+
+        public XmlTypes(string path, string root, string xml)
+        {
+            Path = path;
+            Root = root;
+            Xml = xml;
+        }
+
+    }
+}
